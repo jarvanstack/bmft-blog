af5f25e78199a9f4d1a210afcc2e0cd7f90485b2
dengjiawen <dengjiawen8955@gmail.com>
2023-03-05 17:48:57 +0800

    update: Auto commit And push

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,3 @@
 var repo = 'https://github.com/dengjiawen8955/bmft-blog';
-var keywords = 'docsify,docsify-template,docsify-template,docsify-template';
-var description = 'docsify-template';
+var keywords = 'docsify,docsify-template,博客,半亩方塘,bmft-blog,半亩方塘知识分享,邓嘉文Jarvan,个人博客';
+var description = '邓嘉文Jarvan的个人博客';
