2783cf104eae9b3b32ac2555c37f46e7e77c0327
dengjiawen <dengjiawen8955@gmail.com>
2023-03-12 21:53:30 +0800

    update: Auto commit And push

--- docs/config.js
+++ docs/config.js
@@ -3,13 +3,3 @@ var keywords = 'docsify,docsify-template,博客,半亩方塘,bmft-blog,半亩方
 var description = '邓嘉文Jarvan的个人博客';
 
 var loadSidebar = false;
-
-
-// 百度统计
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?26aafdc51f546a6c9caca26e7174a692";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
