9007ed8e05e6015102ffb6dc97073b31dcb07807
dengjiawen <dengjiawen8955@gmail.com>
2023-04-08 22:29:44 +0800

    update: Auto commit And push

--- update_remote.sh
+++ update_remote.sh
@@ -2,7 +2,7 @@
 set timeout 
 set cmd1 "cd /root/app/bmft-blog && git pull origin master:master"
 # 使用公钥登录不需要密码
-spawn ssh -p 22 root@djv.bmft.tech
+spawn ssh -p 22 root@djv2.bmft.tech
 expect {
     # 发送更新命令
     "*#" {send "$cmd1\r"}
